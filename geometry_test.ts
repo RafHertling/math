1627ee56ddbc697e4b9e744e376d73aecb24c3ce
RafHertling <rafel_hertling@lsuz.ch>
2026-03-20 14:52:24 +0100

    added circle and rectangle tests

--- geometry_test.ts
+++ geometry_test.ts
@@ -12,6 +12,29 @@ Deno.test("circumference of a circle with radius 5 is roughtly 31.416", () => {
   assertAlmostEquals(actual, 31.416, 0.01);
 });
 
+Deno.test("Das ist TAGESLICHT", () => {
+  //Given
+  const circle = new Circle(new Point2D(3, 4), 5);
+
+  //When
+  const actual = circle.area();
+
+  //Then
+  assertAlmostEquals(actual, 7.853, 0.01);
+});
+
+Deno.test("nagelneuer Benzer", () => {
+  //Given
+  const circle = new Circle(new Point2D(3,4), 5);
+
+  //When
+  const actual = circle.diameter();
+  
+  //Then
+  assertEquals(actual, 10);
+});
+
+
 Deno.test("oho da muss ich lachen so schmeckt der", () => {
   // Given
   const recantgle = new Rectangle(new Point2D(3, 5), new Point2D(5, 7));
@@ -28,8 +51,17 @@ Deno.test("Colaaa", () => {
   const recantgle = new Rectangle(new Point2D(3, 5), new Point2D(5, 7));
 
   //When
-  const actual = recantgle.area()
+  const actual = recantgle.area();
 
   //Then
-  assertEquals(actual, 4)
-})
+  assertEquals(actual, 4);
+});
+
+Deno.test("der allerechte Hase", () => {
+  //Given
+  const recantgle = new Rectangle(new Point2D(3, 5), new Point2D(5, 7));
+  //When
+  const actual = recantgle.diagonal();
+  //Then
+  assertAlmostEquals(actual, 2.82, 0.01);
+});
